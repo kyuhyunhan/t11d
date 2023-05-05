020f5569f7937d86f9618cbf1939ad9b9581af09
kyuhyun han <kyuhyunhaan@gmail.com>
2023-05-05 18:14:52 +0900

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<header>
+  <h1>t11d</h1>
+  <nav>
+    <ul>
+      <li><a href="/wiki">wiki</a></li>
+    </ul>
+  </nav>
+  </header>
+  
+  <style>
+    header {
+      display:  flex;
+  
+    }
+  </style>
